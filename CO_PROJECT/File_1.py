1df0ad054c296ef7f1b1e91bcbf2615ae6f277e7
AKIHSNAV <119172207+AKIHSNAV@users.noreply.github.com>
2023-04-27 23:28:20 +0530

    Create File_1.py

---
+++ CO_PROJECT/File_1.py
@@ -0,0 +1,50 @@
+op_code={'add':'00000','sub':'00001','mov':'00010','ld':'00100','st':'00101','mul':'00110','div':'00111','rs':'01000','ls':'01001','xor':'01010','or':'01011','and':'01100','not':'01101','cmp':'01110','jmp':'01111','jlt':'11100','jgt':'11101','je':'11111','hlt':'11010'}
+reg={'R0':'000','R1':'001','R2':'010','R3':'011','R4':'100','R5':'101','R6':'110'}
+
+def type_A(lst):
+    opcode, r1, r2, r3 = lst[0], lst[1], lst[2], lst[3]
+    return op_code.get(opcode) + '00' + reg.get(r1) + reg.get(r2) + reg.get(r3)+'\n'
+
+def type_B(lst):
+    opcode, r1, imm = lst[0], lst[1], lst[2]
+    return op_code.get(opcode) + '0' +  reg.get(r1) + imm + '\n'
+
+def type_C(lst):
+    opcode, r1, r2= lst[0], lst[1], lst[2]
+    return op_code.get(opcode) + '00000' + reg.get(r1) + reg.get(r2) + '\n'
+
+def type_D(lst):
+    opcode, r1, m_add = lst[0], lst[1], lst[2]
+    return op_code.get(opcode) + '0' + reg.get(r1) + m_add  #correct to get memory add and handle error here
+
+def type_E(lst):
+    opcode, m_add= lst[0], lst[1]
+    return op_code.get(opcode) + '0000' +m_add
+
+def type_F(lst):
+    opcode= lst[0]
+    return op_code.get(opcode) + '00000000000'
+line='xxxxx'
+
+def identify_type(lst):
+    if len(lst)==4:
+        print(type_A(lst))
+    elif len(lst)==2:
+        print(type_E(lst))
+    elif len(lst)==1:
+        print(type_F(lst))
+    else:
+        if '$' in lst[-1]:
+            print(type_B(lst))
+        elif 'r' in lst[-1]:
+            print(type_C(lst))
+        elif lst[0] == 'ld' or lst[0] == 'st':
+            print(type_D(lst))
+
+f = open('input.txt', 'r')
+page = f.read()
+lines = page.split('\n')
+print(lines)
+for line in lines:
+    words = line.split()
+    identify_type(words)
